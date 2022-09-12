fadf65320d059ba14173b1657f694c2c7a5b1d47
lhy <dev@lhy.kr>
2022-09-12 14:40:56 +0900

    [pystagram] 17.3. 피드페이지
    - 이미지 슬라이더(Splide) 구현
    - 작성자 정보, 글 내용, 작성일시, 댓글목록, 댓글입력 구조 구현

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 
+from posts.models import Post
+
 
 def feeds(request):
     user = request.user
     if not user.is_authenticated:
         return redirect("/users/login/")
-    return render(request, "posts/feeds.html")
+
+    posts = Post.objects.all()
+    context = {"posts": posts}
+    return render(request, "posts/feeds.html", context)
