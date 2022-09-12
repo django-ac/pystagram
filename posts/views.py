8934488f1a34437076acc2e34f919eaa4ef0471f
lhy <dev@lhy.kr>
2022-09-12 17:18:10 +0900

    [pystagram] 20.3. 해시태그 검색
    - 해시태그 검색결과 Grid구현
    - 없는 해시태그명으로 검색 시 빈 페이지 표시
    - 피드페이지에서 해시태그 검색결과로의 링크 추가

--- posts/views.py
+++ posts/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from posts.forms import CommentForm, PostForm
-from posts.models import Post, Comment, PostImage
+from posts.models import Post, Comment, PostImage, HashTag
 
 
 def feeds(request):
@@ -98,3 +98,21 @@ def post_add(request):
 
     context = {"form": form}
     return render(request, "posts/post_add.html", context)
+
+
+def tags(request, tag_name):
+    try:
+        tag = HashTag.objects.get(name=tag_name)
+    except HashTag.DoesNotExist:
+        # tag_name에 해당하는 HashTag를 찾지 못한 경우 빈 QuerySet을 돌려준다
+        posts = Post.objects.none()
+    else:
+        posts = Post.objects.filter(tags=tag)
+
+    # context로 Template에 필터링 된 Post QuerySet을 넘겨주며,
+    # 어떤 tag_name으로 검색했는지도 넘겨준다
+    context = {
+        "tag_name": tag_name,
+        "posts": posts,
+    }
+    return render(request, "posts/tags.html", context)
